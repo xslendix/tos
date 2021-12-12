227b0532b8657ed6cab2f52fc904b60f8eb5f221
xSlendiX <gamingxslendix@gmail.com>
2021-12-12 14:05:35 +0200

    Fix (un)mounting scripts to only look for TOS

--- umount.sh
+++ umount.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check if image already mounted
-mounted="$(lsblk | grep -c 'loop0')"
+mounted="$(lsblk | grep -c 'loop.*mnt.*')"
 if [ "$mounted" -eq 0 ]; then
     echo "[INFO] Partition already unmounted. Ignoring request."
     exit
